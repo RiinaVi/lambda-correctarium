27925356d779eb9d544d07a99acfdc5e5ad2841a
RiinaVi <riinavi86@gmail.com>
2020-03-15 16:03:28 +0200

    make deadline calculator

--- src/calculators.js
+++ src/calculators.js
@@ -1,31 +1,28 @@
-import zeropad from 'zeropad';
+import moment from "moment";
 
-export function formatDate(date) {
-    let properDate = new Date(date);
-    return zeropad(properDate.getDate()) + '/'
-        + zeropad(properDate.getMonth() + 1) + '/'
-        + properDate.getFullYear() +' '
-        + zeropad(properDate.getHours()) +':'+ zeropad(properDate.getMinutes());
-}
 export const languages = [
     'Russian', 'English', 'Ukrainian'
 ];
 
 const allPricesForSymbol = {
-    russian: 0.05,
-    ukrainian: 0.05,
-    english: 0.12
+    Russian: 0.05,
+    Ukrainian: 0.05,
+    English: 0.12
 };
 
 const allSymbolsPerHour = {
-    russian: 1333,
-    ukrainian: 1333,
-    english: 333
+    Russian: 1333,
+    Ukrainian: 1333,
+    English: 333
 };
 
+function formatDate(date) {
+    return moment(date).format('DD/MM/YYYY HH:mm')
+}
+
 export function priceCalculator(textLength, lang) {
-    if(!textLength) return 0;
-    let priceForOneSymbol = allPricesForSymbol[lang.toLowerCase()];
+    if (!textLength) return 0;
+    let priceForOneSymbol = allPricesForSymbol[lang];
     let minimumPrice = priceForOneSymbol * 1000;
     let price = priceForOneSymbol * textLength;
     return price >= minimumPrice ? price : minimumPrice;
@@ -33,22 +30,58 @@ export function priceCalculator(textLength, lang) {
 
 export function timeCalculator(textLength, lang) {
     let minimumTime = 60;
-    let symbolsPerHour = allSymbolsPerHour[lang.toLowerCase()];
-    if (textLength<=symbolsPerHour) return minimumTime;
+    let symbolsPerHour = allSymbolsPerHour[lang];
+    if (textLength <= symbolsPerHour) return minimumTime;
 
-    let symbolPerMinute = Math.round(symbolsPerHour/60);
-    let time = 30 + Math.round(textLength/symbolPerMinute);
+    let symbolPerMinute = Math.round(symbolsPerHour / 60);
+    let time = 30 + Math.round(textLength / symbolPerMinute);
     console.log(time);
     return time;
 }
 
-export function deadlineCalculator(timeForWork, orderTime='') {
-    let deadline = orderTime ? new Date(orderTime) : new Date();
-    deadline.setMinutes(deadline.getMinutes()+timeForWork);
-    return formatDate(deadline);
+export function deadlineCalculator(timeForWork, orderTime = new Date()) {
+
+    orderTime = toNextWorkingMoment(orderTime);
+    const timeToEnd = moment.duration(moment(orderTime).hours(19).diff(orderTime)).asMinutes();
+
+    if (timeToEnd >= timeForWork) {
+        orderTime = moment(orderTime).add(timeForWork, 'minutes');
+        return formatDate(orderTime);
+    }
+
+    orderTime = moment(orderTime).add(timeToEnd, 'minutes');
+    return deadlineCalculator(timeForWork - timeToEnd, orderTime)
 }
 
 
-// priceCalculator(process.argv[2].length, urk)
-// timeCalculator(133, languages[1]);
-// module.exports = {priceCalculator, deadlineCalculator, timeCalculator};
+function toNextWorkingMoment(orderTime) {
+
+    const isAfterFinish = moment(orderTime).isSameOrAfter(moment(orderTime).hours(19));
+    const isBeforeStart = moment(orderTime).isBefore(moment(orderTime).hours(10));
+    const isFriday = moment(orderTime).day() === 5;
+    const isSaturday = moment(orderTime).day() === 6;
+    const isSunday = moment(orderTime).day() === 0;
+
+    function addDays(days) {
+        return moment(orderTime).add(days, 'days').hours(10).minutes(0)
+    }
+
+    if (isFriday && isAfterFinish) {
+        return addDays(3)
+    }
+
+    if (isAfterFinish || isSunday) {
+        return addDays(1)
+    }
+
+    if (isSaturday) {
+        return addDays(2)
+    }
+
+    if (isBeforeStart) {
+        return addDays(0)
+    }
+
+    return orderTime
+}
+
