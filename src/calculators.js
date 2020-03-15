044344b067f357c738eb7dfd0c4fb9f137128df6
RiinaVi <riinavi86@gmail.com>
2020-03-15 20:04:39 +0200

    fix deadline calculator

--- src/calculators.js
+++ src/calculators.js
@@ -16,37 +16,37 @@ const allSymbolsPerHour = {
     English: 333
 };
 
+const startOfWork = 10;
+const endOfWork = 19;
+
 function formatDate(date) {
     return moment(date).format('DD/MM/YYYY HH:mm')
 }
 
 export function priceCalculator(textLength, lang) {
     if (!textLength) return 0;
-    let priceForOneSymbol = allPricesForSymbol[lang];
-    let minimumPrice = priceForOneSymbol * 1000;
-    let price = priceForOneSymbol * textLength;
+    const priceForOneSymbol = allPricesForSymbol[lang];
+    const minimumPrice = priceForOneSymbol * 1000;
+    const price = priceForOneSymbol * textLength;
     return price >= minimumPrice ? price : minimumPrice;
 }
 
 export function timeCalculator(textLength, lang) {
-    let minimumTime = 60;
-    let symbolsPerHour = allSymbolsPerHour[lang];
+    const minimumTime = 60;
+    const symbolsPerHour = allSymbolsPerHour[lang];
     if (textLength <= symbolsPerHour) return minimumTime;
 
-    let symbolPerMinute = Math.round(symbolsPerHour / 60);
-    let time = 30 + Math.round(textLength / symbolPerMinute);
-    console.log(time);
-    return time;
+    const symbolPerMinute = symbolsPerHour / 60;
+    return 30 + Math.ceil(textLength / symbolPerMinute);
 }
 
 export function deadlineCalculator(timeForWork, orderTime = new Date()) {
 
     orderTime = toNextWorkingMoment(orderTime);
-    const timeToEnd = moment.duration(moment(orderTime).hours(19).diff(orderTime)).asMinutes();
+    const timeToEnd = moment.duration(moment(orderTime).hours(endOfWork).minutes(0).diff(orderTime)).asMinutes();
 
     if (timeToEnd >= timeForWork) {
-        orderTime = moment(orderTime).add(timeForWork, 'minutes');
-        return formatDate(orderTime);
+        return formatDate(moment(orderTime).add(timeForWork, 'minutes'));
     }
 
     orderTime = moment(orderTime).add(timeToEnd, 'minutes');
@@ -56,26 +56,26 @@ export function deadlineCalculator(timeForWork, orderTime = new Date()) {
 
 function toNextWorkingMoment(orderTime) {
 
-    const isAfterFinish = moment(orderTime).isSameOrAfter(moment(orderTime).hours(19));
-    const isBeforeStart = moment(orderTime).isBefore(moment(orderTime).hours(10));
+    const isAfterFinish = moment(orderTime).isSameOrAfter(moment(orderTime).hours(endOfWork).minutes(0));
+    const isBeforeStart = moment(orderTime).isBefore(moment(orderTime).hours(startOfWork).minutes(0));
     const isFriday = moment(orderTime).day() === 5;
     const isSaturday = moment(orderTime).day() === 6;
     const isSunday = moment(orderTime).day() === 0;
 
     function addDays(days) {
-        return moment(orderTime).add(days, 'days').hours(10).minutes(0)
+        return moment(orderTime).add(days, 'days').hours(startOfWork).minutes(0)
     }
 
     if (isFriday && isAfterFinish) {
         return addDays(3)
     }
 
-    if (isAfterFinish || isSunday) {
-        return addDays(1)
-    }
-
     if (isSaturday) {
         return addDays(2)
+    }
+
+    if (isAfterFinish || isSunday) {
+        return addDays(1)
     }
 
     if (isBeforeStart) {
